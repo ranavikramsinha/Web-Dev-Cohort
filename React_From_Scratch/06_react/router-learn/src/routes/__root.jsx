973f61f0807211fa0c8b2da31f72371f73e41d80
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2025-06-01 19:42:33 +0530

    zustand and nextjs

--- React_From_Scratch/06_react/router-learn/src/routes/__root.jsx
+++ React_From_Scratch/06_react/router-learn/src/routes/__root.jsx
@@ -3,13 +3,17 @@ import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { Link } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { useNotification } from '../context/NotificationContext'
+import { useCartStore } from '../store/cartStore'
 
 export const Route = createRootRoute({
   component: RootComponent,
 })
 
 function RootComponent() {
-  const {count} = useNotification()
+  const {count} = useNotification();
+  const cartCount = useCartStore((state) => state.cart.length)
+  const addToCart = useCartStore((state) => state.addToCart)
+
   return (
     <React.Fragment>
       <div>Hello "__root"!</div>
